811089be394f958ff41ea5d59b39f57a4890754a
cdeboor <2015cdeboor@tjhsst.edu>
2015-02-06 17:48:31 -0500

    Added ray trace implementation

---
+++ raycast.c
@@ -0,0 +1,105 @@
+#include "math.h"
+#include "raycast.h"
+#include "raytrace.h"
+
+float castShape(Shape *s, vec3 *pos, vec3 *dir);
+	switch (shp->type) {
+		case SHAPE_SPHERE:
+			return castSphere(&s->sphere, pos, dir);
+		case SHAPE_PLANE:
+			return castPlane(&s->plane, pos, dir);
+		case SHAPE_TRIANGLE:
+			return castTriangle(&s->triangle, pos, dir);
+		default:
+			return INFINITY;
+	}
+}
+
+float castSphere(Sphere *s, vec3 *pos, vec3 *dir) {
+	float b, c, d;
+	
+	vec3 dif;
+
+	subtract(&dif, pos, &s->pos);
+	b = dot(&dif, dir);
+	c = dot(pos, pos) + dot(&s->pos, &s->pos) - 2*dot(pos, &s->pos);
+
+	d = b*b - 4*c;
+
+	if (d < 0) return INFINITY;
+
+	return -(b+sqrt(d))/2;
+}
+
+float castPlane(Plane *s, vec3 *pos, vec3 *dir) {
+	vec3 *n = &s->norm;
+	return (dot(n, &s->pos) - dot(n, pos)) / (dot(n, dir));
+}
+
+float lineSide(vec3 *n, vec3 *a, vec3 *b, vec3 *p) {
+	vec3 line, dir;
+	subtract(&line, b, a);
+	subtract(&dir, p, a);
+	cross(&line, &line, &dir);
+	return dot(n, &line);
+}
+
+float castTriangle(Triangle *s, vec3 *pos, vec3 *dir) {
+	vec3 n, p;
+	norm(&n, s, &s->a);
+
+	float t = (dot(&n, &s->a) - dot(&n, pos)) / (dot(&n, dir));
+
+	p = *dir;
+	scale(&p, t);
+	add(&p, &p, pos);
+
+	float s1, s2, s3;
+	s1 = lineSide(&n, &s->a, &s->b, &p);
+	s2 = lineSide(&n, &s->b, &s->c, &p);
+	s3 = lineSide(&n, &s->c, &s->a, &p);
+
+	return s1*s2 >= 0 && s1*s3 >= 0 ? t : INFINITY;
+}
+
+Shape *rayTrace(vec3 *hit, Scene *scene, int ignoreCount, Shape **ignore, vec3 *src, vec3 *dest) {
+	int i, j, skip;
+
+	vec3 r;
+	float t;
+
+	float bestT = INFINITY;
+	Shape *bestShape;
+	vec3 bestHit;
+
+	subtract(&r, dest, src);
+	normalize(&r);
+
+	for (i=scene->shapeCount;i-->0;) {
+		skip = 0;
+		for (j=ignoreCount;j-->0;) {
+			if (ignore[j] == &scene->shapes[i]) {
+				skip = 1;
+				break;
+			}
+		}
+		if (skip) continue;
+		
+		t = castShape(&scene->shapes[i], src, &r);
+
+		if (t >= 0 && t < bestT) {
+			bestHit = r;
+			scale(&bestHit, t);
+			add(&bestHit, &bestHit, src);
+			bestShape = &scene->shapes[i];
+		}
+	}
+
+	if (bestT < INFINITY) {
+		*hit = bestHit;
+		return bestShape;
+	} else {
+		return NULL;
+	}
+}
+
---
+++ raycast.h
@@ -0,0 +1,14 @@
+#ifndef RAYCAST_H
+#define RAYCAST_H
+
+#include "raytrace.h"
+#include "shape.h"
+#include "vector.h"
+
+float castShape(Shape *s, vec3 *pos, vec3 *dir);
+float castSphere(Sphere *s, vec3 *pos, vec3 *dir);
+float castPlane(Plane *s, vec3 *pos, vec3 *dir);
+float castTriangle(Triangle *s, vec3 *pos, vec3 *dir);
+
+#endif
+
