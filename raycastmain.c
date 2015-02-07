e75dc2e04fdca5c37d7618331bd9d68e093eddbd
cdeboor <2015cdeboor@tjhsst.edu>
2015-02-06 22:42:38 -0500

    Update struct initializer

--- raycastmain.c
+++ raycastmain.c
@@ -3,54 +3,63 @@
 #include <string.h>
 #include <math.h>
 
+#include "scene.h"
+#include "image.h"
+
 void imageSave(FILE *out, ImageData *img) {
 	fprintf(out, "P6 %d %d 255\n", img->width, img->height);
 
-	fwrite(img->pixels, sizeof(Pixel), img->width*img->height, out);
+	fwrite(img->pixels, sizeof(Color), img->width*img->height, out);
 }
 
 #define WIDTH 800
 #define HEIGHT 600
 
-Scene scene = {
-	.lightCount = 1,
-	.lights = {
-		{ .pointLight = {
-			.type = LIGHT_POINT_SOURCE,
-			.pos = { 0, 1, -0.5 },
-			.lightColor = { 255, 255, 255 },
-		} }
+Scene scene = (Scene) {
+	.shapeCount = 3,
+	.shapes = (Shape []) {
+		{
+			.sphere = {
+				.type = SHAPE_SPHERE,
+				.pos = {0.5,0.5,0.166667},
+				.radius = 0.166667,
+			}
+		},
+		{
+			.sphere = {
+				.type = SHAPE_SPHERE,
+				.pos = {0.8333333,0.5,0.5},
+				.radius = 0.166667,
+			}
+		},
+		{
+			.sphere = {
+				.type = SHAPE_SPHERE,
+				.pos = {0.333333,0.666667,0.666667},
+				.radius = 0.333333,
+			}
+		},
 	},
 
-	.shapeCount = 3,
-	.shapes = {
-		{ .sphere = {
-			.type = SHAPE_SPHERE,
-			.pos = {0.5,0.5,0.166667},
-			.radius = 0.166667,
-		} },
-		{ .sphere = {
-			.type = SHAPE_SPHERE,
-			.pos = {0.8333333,0.5,0.5},
-			.radius = 0.166667,
-		} },
-		{ .sphere = {
-			.type = SHAPE_SPHERE,
-			.pos = {0.333333,0.666667,0.666667},
-			.radius = 0.333333,
-		} },
-	}
-}
+	.lightCount = 1,
+	.lights = (Light []) {
+		{
+			.pointLight = {
+				.type = LIGHT_POINT_SOURCE,
+				.pos = { 0.0, 1.0, -0.5 },
+				.lightColor = { 255, 255, 255 },
+			}
+		},
+	},
+};
 
 int main(int argc, char **argv) {
 	ImageData *img;
 
 	img = imageAlloc(WIDTH, HEIGHT);
 
-	vec3 eye = {0.5, 0.5, -1};
-	vec3 screen = {0.5, 0.5, 0};
+	cameraInit(&scene.camera, &((vec3) {0.5, 0.5, -1}), &((vec3) {0.5, 0.5, 0}), 0, 1, 1);
 
-	cameraInit(&scene.camera, &eye, &screen, 0, 1, 1);
 	
 	sceneRender(img, &scene);
 
