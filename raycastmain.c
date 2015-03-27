2521603879f32413499e7d05fa8bf9133bfbd4e8
cdeboor <2015cdeboor@tjhsst.edu>
2015-03-27 12:51:47 -0400

    Switch to loading scene from file

--- raycastmain.c
+++ raycastmain.c
@@ -16,104 +16,18 @@ void imageSave(FILE *out, ImageData *img) {
 #define WIDTH 800
 #define HEIGHT 600
 
-#define WHITE { 1, 1, 1 }
-
-Scene scene = {
-	.objectCount = 4,
-	.objects = (SceneObject []) {
-		{
-			.shape = { .sphere = {
-				.type = SHAPE_SPHERE,
-				.pos = {0.5,0.5,0.166667},
-				.radius = 0.166667,
-			} },
-			.material = { .constant = {
-				.type = MATERIAL_CONSTANT,
-				.specularness = 0.5,
-				.fresnel = 0.2,
-				.roughness = 0.2,
-				.diffuse = { 0, 0, 1 },
-				.specular = WHITE,
-			} },
-		},
-		{
-			.shape = { .sphere = {
-				.type = SHAPE_SPHERE,
-				.pos = {0.8333333,0.5,0.5},
-				.radius = 0.166667,
-			} },
-			.material = { .constant = {
-				.type = MATERIAL_CONSTANT,
-				.specularness = 0.5,
-				.fresnel = 0.2,
-				.roughness = 0.2,
-				.diffuse = { 0, 1, 0 },
-				.specular = WHITE,
-			} },
-		},
-		{
-			.shape = { .sphere = {
-				.type = SHAPE_SPHERE,
-				.pos = {0.333333,0.666667,0.666667},
-				.radius = 0.333333,
-			} },
-			.material = { .constant = {
-				.type = MATERIAL_CONSTANT,
-				.specularness = 0.5,
-				.fresnel = 0.2,
-				.roughness = 0.2,
-				.diffuse = { 1, 0, 0 },
-				.specular = WHITE,
-			} },
-		},
-		{
-			.shape = { .plane = {
-				.type = SHAPE_PLANE,
-				.pos = {0, 0.333333, 0},
-				.norm = {0, 1, 0},
-			} },
-			.material = { .checkered = {
-				.type = MATERIAL_CHECKERED,
-				.specularness = 0.5,
-				.fresnel = 0.2,
-				.roughness = 0.2,
-				.checkSize = 0.1,
-				.diffuse1 = { 1, 1, 0 },
-				.diffuse2 = { 0, 1, 1 },
-				.specular1 = WHITE,
-				.specular2 = WHITE,
-			} },
-		},
-	},
-
-	.lightCount = 2,
-	.lights = (Light []) {
-		{
-			.pointLight = {
-				.type = LIGHT_POINT_SOURCE,
-				.pos = { 0.0, 1.0, -0.5 },
-				.power = 4,
-				.lightColor = { 1, 1, 1 },
-			}
-		},
-		{
-			.pointLight = {
-				.type = LIGHT_POINT_SOURCE,
-				.pos = { 1.0, 1.0, -0.5 },
-				.power = 2,
-				.lightColor = { 0.8, 0, 1 },
-			}
-		},
-	},
-};
-
 int main(int argc, char **argv) {
 	ImageData *img;
 
 	img = imageAlloc(WIDTH, HEIGHT);
 
-	cameraInit(&scene.camera, &((vec3) {0.5, 0.5, -1}), &((vec3) {0.5, 0.5, 0}), &((vec3) {0, 1, 0}), 1.0*WIDTH/HEIGHT, 1);
-	sceneRender(img, &scene);
+	Scene loaded;
+	if (sceneLoad(&loaded, "scene1.yml")) {
+		fprintf(stderr, "Failed to load\n");
+		return 1;
+	}
+
+	sceneRender(img, &loaded);
 
 	FILE *out;
 	out = fopen("raycast.ppm", "w");
@@ -121,5 +35,7 @@ int main(int argc, char **argv) {
 	fclose(out);
 
 	imageFree(img);
+
+	return 0;
 }
 
